e106d1c0ee106eb1cca1e9525ad4e13777a4646f
kedixa <1204837541@qq.com>
2023-03-05 00:33:47 +0800

    feature: TcpAdaptor TcpAdaptor: Async adaptor to communicate with tcp

--- pykedixa/comm/socket_adaptor.py
+++ pykedixa/comm/socket_adaptor.py
@@ -1,4 +1,7 @@
+import asyncio
 import socket
+
+from .. import compat
 
 from .basic import (
     AdaptorEofError,
@@ -12,6 +15,7 @@ from .basic import (
 
 __all__ = [
     'SyncTcpAdaptor',
+    'TcpAdaptor',
 ]
 
 
@@ -44,3 +48,53 @@ class SyncTcpAdaptor(BasicAdaptor):
 
     async def write(self, buffer: ReadableBuffer) -> int:
         return self._socket.send(buffer)
+
+class TcpAdaptor(BasicAdaptor):
+    def __init__(self, addr, *, family=socket.AF_INET, proto=0):
+        self._addr          = addr
+        self._family: int   = family
+        self._proto: int    = proto
+        self._reader: asyncio.StreamReader = None
+        self._writer: asyncio.StreamWriter = None
+
+    async def prepare(self):
+        addr = self._addr
+        self._reader, self._writer = await asyncio.open_connection(
+            host=addr[0], port=addr[1], family=self._family,
+            proto=self._proto
+        )
+
+    async def finish(self):
+        if self._writer.can_write_eof():
+            self._writer.write_eof()
+
+        self._writer.close()
+
+        if compat.PY37:
+            await self._writer.wait_closed()
+
+    async def read(self, max_bytes: int = -1, *,
+            buffer: WritableBuffer = None) -> ReadRetType:
+        if max_bytes < 0:
+            max_bytes = DEFAULT_MAX_READ_SIZE
+
+        data = await self._reader.read(max_bytes)
+
+        if not data:
+            raise AdaptorEofError('TcpAdaptorEof')
+
+        if buffer:
+            dlen = len(data)
+            buffer[:dlen] = data
+            return dlen
+        else:
+            return data
+
+    async def write(self, buffer: ReadableBuffer) -> int:
+        blen = len(buffer)
+
+        self._writer.write(buffer)
+        return blen
+
+    async def flush(self):
+        await self._writer.drain()
